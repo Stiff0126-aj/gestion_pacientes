02547c26783802343b78ccf34b8aac7efd538640
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-05-25 18:27:47 -0500

    url

--- foro/urls.py
+++ foro/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('paciente/<int:paciente_id>/foros/', views.foro_paciente, name='foro_paciente'),
-    path('paciente/<int:paciente_id>/crear_foro/', views.crear_foro, name='crear_foro'),
+    path('<int:paciente_id>/foros/', views.foro_paciente, name='foro_paciente'),
+    path('<int:paciente_id>/crear_foro/', views.crear_foro, name='crear_foro'),
 ]
