adc23da8a70bc3c379e1699e69afefeabb9eaa9f
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-05-26 10:43:59 -0500

    Chambonada

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -3,7 +3,8 @@ from .views import home, paciente_list, paciente_historial
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from foro import views as foro_views
+from historias_clinicas import views as historias_views
 urlpatterns = [
     
     path('', home, name='home'), 
@@ -11,13 +12,14 @@ urlpatterns = [
     path('crear/', views.paciente_create, name='paciente_create'),
     path('paciente/historial/<int:paciente_id>/', views.paciente_historial, name='paciente_historial'),
     path('paciente/eliminar/<int:paciente_id>/', views.paciente_delete, name='paciente_delete'),
-    path('health/', views.healthCheck, name= 'health'),		
+    path('health/', views.healthCheck, name= 'health'),	
+path('<int:paciente_id>/foros/', foro_views.foro_paciente, name='foro_paciente'),
+    path('<int:paciente_id>/crear_foro/', foro_views.crear_foro, name='crear_foro'),
+     path('api/historias/', historias_views.historia_list, name='historia_list'),
+    path('api/historia/crear/<int:paciente_id>/', historias_views.crear_historia, name='crear_historia'),
+
     
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [
-   path('historias/', include('historias_clinicas.urls')),
-    path('foro/', include('foro.urls'))
-]
