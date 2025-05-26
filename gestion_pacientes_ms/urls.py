d25cd4e9e273296162e1aa4ed114b48f68fdb883
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-05-26 06:55:46 -0500

    foro

--- gestion_pacientes_ms/urls.py
+++ gestion_pacientes_ms/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('pacientes/', include('pacientes.urls')),
     path('historias/', include('historias_clinicas.urls')),
+    path('pacientes/foro/paciente/', include('foro.urls')),
 ]
