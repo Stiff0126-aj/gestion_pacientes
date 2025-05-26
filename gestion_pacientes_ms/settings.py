34f4fe5913dcebcf83fbb55c43570cd4bf35618a
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-05-26 10:38:18 -0500

    urls

--- gestion_pacientes_ms/settings.py
+++ gestion_pacientes_ms/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'gestion_pacientes_ms',
     'pacientes',
     'historias_clinicas',
     'foro',
