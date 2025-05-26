a73507799e74872eb41fef1d981d1e4fda6107ca
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-05-26 11:06:09 -0500

    Revert "DATA"
    This reverts commit 68054d16943b69b8176cc3f2c8d8cd82625dd49b.

--- gestion_pacientes_ms/settings.py
+++ gestion_pacientes_ms/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'gestion_pacientes_ms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '10.20.176.3',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
